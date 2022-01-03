58f526502e5a82af5a3dab5048db668a7db60579
Thiago Sagara <30393842+thiagosagara@users.noreply.github.com>
2022-01-03 18:59:43 -0300

    Update config.py
    Alteração para o Lab

--- config.py
+++ config.py
@@ -8,10 +8,10 @@ BASE_DIR = os.path.abspath('.')
 
 DEBUG = config('DEBUG', cast=bool)
 
-SECRET_KEY = config('SECRET_KEY') or \
-    ''.join(random.choice(string.ascii_letters) for i in range(42))
+#SECRET_KEY = config('SECRET_KEY') or \
+SECRET_KEY = ''.join(random.choice(string.ascii_letters) for i in range(42))
 
-SQLALCHEMY_DATABASE_URI = config('SQLALCHEMY_DATABASE_URI') or \
-    'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite')
+#SQLALCHEMY_DATABASE_URI = config('SQLALCHEMY_DATABASE_URI') or \
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite')
 
 SQLALCHEMY_TRACK_MODIFICATIONS = config('SQLALCHEMY_TRACK_MODIFICATIONS', True, cast=bool)
